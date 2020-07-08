cd4f70e9bf103a2e1cf44ed9c70bd7413181101f
mo-alaa88 <mohammed.alaa.al.din@gmail.com>
2020-07-08 03:04:01 +0200

    sdg

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+import BrowserDetection from 'react-browser-detection';
+ 
+const browserHandler = {
+  chrome: () => <div>Chrome is fantastic!</div>,
+  googlebot: () => <div>Hi GoogleBot!</div>,
+  default: (browser) => <div>Hi {browser}!</div>,
+};
+ 
+export default class App extends Component {
+  render() {
+    return (
+      <BrowserDetection>
+        { browserHandler }
+      </BrowserDetection>
+    );
+  }
 }
-
-export default App;
